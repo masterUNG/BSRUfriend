a0cf71bf1b6891992af1bc39dab3300cd36a5b09
masterUNG <phrombutr@gmail.com>
2017-02-10 10:13:06 +0700

    Design UI SignUp OK4

---
+++ app/src/main/java/appewtc/masterung/bsrufriend/MyAlert.java
@@ -0,0 +1,20 @@
+package appewtc.masterung.bsrufriend;
+
+import android.content.Context;
+
+/**
+ * Created by masterUNG on 2/10/2017 AD.
+ */
+
+public class MyAlert {
+
+    //Explicit
+    private Context context;
+
+    public MyAlert(Context context) {
+        this.context = context;
+    }   // Constructor
+
+
+
+}   // Main Class

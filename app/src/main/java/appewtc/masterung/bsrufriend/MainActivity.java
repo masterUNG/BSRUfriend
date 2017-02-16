9d2d889cc05cfda9095d5c6d508fb8eeb2411c48
masterUNG <phrombutr@gmail.com>
2017-02-16 11:42:21 +0700

    Check User OK

--- app/src/main/java/appewtc/masterung/bsrufriend/MainActivity.java
+++ app/src/main/java/appewtc/masterung/bsrufriend/MainActivity.java
@@ -8,15 +8,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
 
     //Explicit การประกาศตัวแปร
     private Button signInButton, signUpButton;
     private EditText userEditText, passEditText;
     private String userString, passString;
-    private String[] loginStrings;
+    private String[] loginStrings = new String[8];
     private static final String urlPHP = "http://swiftcodingthai.com/bsru/get_user_master.php";
-
+    private boolean aBoolean = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,6 +71,33 @@ public class MainActivity extends AppCompatActivity {
             getUser.execute(urlPHP);
             String strJSON = getUser.get();
             Log.d("16febV1", "strJSON ==> " + strJSON);
+
+            JSONArray jsonArray = new JSONArray(strJSON);
+            for (int i = 0; i < jsonArray.length(); i += 1) {
+
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                if (userString.equals(jsonObject.getString("User"))) {
+
+                    loginStrings[0] = jsonObject.getString("id");
+                    loginStrings[1] = jsonObject.getString("Name");
+                    loginStrings[2] = jsonObject.getString("User");
+                    loginStrings[3] = jsonObject.getString("Password");
+                    loginStrings[4] = jsonObject.getString("Image");
+                    loginStrings[5] = jsonObject.getString("Avata");
+                    loginStrings[6] = jsonObject.getString("Lat");
+                    loginStrings[7] = jsonObject.getString("Lng");
+
+                    aBoolean = false;
+
+                }   // if
+
+            }   // for
+
+            if (aBoolean) {
+                //User False
+                MyAlert myAlert = new MyAlert(MainActivity.this);
+                myAlert.myDialog("หา User นี่ไม่เจอ ?", "ไม่มี " + userString + " ในฐานข้อมูลของเรา");
+            }
 
 
         } catch (Exception e) {

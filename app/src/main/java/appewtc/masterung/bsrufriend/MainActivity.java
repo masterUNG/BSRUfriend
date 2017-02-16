3a97a66da8f92ba5ce2053b49b08145205b0cf6e
masterUNG <phrombutr@gmail.com>
2017-02-16 11:55:02 +0700

    Check Password OK

--- app/src/main/java/appewtc/masterung/bsrufriend/MainActivity.java
+++ app/src/main/java/appewtc/masterung/bsrufriend/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -97,6 +98,16 @@ public class MainActivity extends AppCompatActivity {
                 //User False
                 MyAlert myAlert = new MyAlert(MainActivity.this);
                 myAlert.myDialog("หา User นี่ไม่เจอ ?", "ไม่มี " + userString + " ในฐานข้อมูลของเรา");
+            } else if (!passString.equals(loginStrings[3])) {
+                //Password False
+                MyAlert myAlert = new MyAlert(MainActivity.this);
+                myAlert.myDialog("Password False", "Please Try Again Password False");
+
+            } else {
+                //Password True
+                Toast.makeText(MainActivity.this, "Welcome " + loginStrings[1],
+                        Toast.LENGTH_SHORT).show();
+
             }
 
 

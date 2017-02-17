62e74b8ca6dfc9dc57938f6760f3667dd09459a6
masterUNG <phrombutr@gmail.com>
2017-02-17 16:21:34 +0700

    Finish 17feb

--- app/src/main/java/appewtc/masterung/bsrufriend/ListFriend.java
+++ app/src/main/java/appewtc/masterung/bsrufriend/ListFriend.java
@@ -1,8 +1,11 @@
 package appewtc.masterung.bsrufriend;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import org.json.JSONArray;
@@ -26,10 +29,10 @@ public class ListFriend extends AppCompatActivity {
             String strJSON = getUser.get();
 
             JSONArray jsonArray = new JSONArray(strJSON);
-            String[] nameStrings = new String[jsonArray.length()];
-            String[] imageStrings = new String[jsonArray.length()];
-            String[] latStrings = new String[jsonArray.length()];
-            String[] lngStrings = new String[jsonArray.length()];
+            final String[] nameStrings = new String[jsonArray.length()];
+            final String[] imageStrings = new String[jsonArray.length()];
+            final String[] latStrings = new String[jsonArray.length()];
+            final String[] lngStrings = new String[jsonArray.length()];
 
             for (int i=0;i<jsonArray.length();i++) {
 
@@ -44,6 +47,20 @@ public class ListFriend extends AppCompatActivity {
             FriendAdapter friendAdapter = new FriendAdapter(ListFriend.this,
                     imageStrings, nameStrings);
             listView.setAdapter(friendAdapter);
+
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                    Intent intent = new Intent(ListFriend.this, DetailFriend.class);
+                    intent.putExtra("Name", nameStrings[i]);
+                    intent.putExtra("Image", imageStrings[i]);
+                    intent.putExtra("Lat", latStrings[i]);
+                    intent.putExtra("Lng", lngStrings[i]);
+                    startActivity(intent);
+
+                }
+            });
 
 
 
---
+++ app/src/main/java/appewtc/masterung/bsrufriend/MyConstant.java
@@ -0,0 +1,15 @@
+package appewtc.masterung.bsrufriend;
+
+/**
+ * Created by masterUNG on 2/17/2017 AD.
+ */
+
+public class MyConstant {
+
+    //Explicit
+    private String urlPHPString = "http://swiftcodingthai.com/bsru/get_user_master.php";
+
+    public String getUrlPHPString() {
+        return urlPHPString;
+    }
+}   // Main Class
